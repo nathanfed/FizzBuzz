dc8cc8e84b9c4917ea725abefddf0425d538c7b7
nathanfed <45716579+nathanfed@users.noreply.github.com>
2018-12-08 11:36:00 -0800

    Add files via upload

---
+++ FizzBuzz.swift
@@ -0,0 +1,23 @@
+//
+//  FizzBuzz.swift
+//  
+//
+//  Created by Nathan Federman on 12/8/18.
+//
+
+import Foundation
+
+for i in 1...100 {
+    if i % 15 == 0 {
+        print("FizzBuzz!!!")
+    }
+    else if i % 5 == 0 {
+        print("Buzz")
+    }
+    else if i % 3 == 0 {
+        print("Fizz")
+    }
+    else {
+        print (i)
+    }
+}
